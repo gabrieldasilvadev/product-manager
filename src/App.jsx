f8dfadaeb01d279e7902f1e27eec3efb385f2c03
Gabriel da Silva <gabrieldasilvaprivado1@gmail.com>
2022-01-14 04:40:36 -0300

    Principal component for render

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="app">
+      <center>
+        <Header></Header>
+        <h1>Hello World</h1>
+      </center>
+    </div>
+  );
+}
+
+export default App;
