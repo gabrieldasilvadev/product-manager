cce9af847be3d4f13ac68c4e82c14babddf07b95
Gabriel da Silva <gabrieldasilvaprivado1@gmail.com>
2022-01-16 00:30:08 -0300

    Custom div to encapsulate the form

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,12 +1,32 @@
 import React from 'react';
 import { Button, InputText } from '../../styles/FormStyles';
+import { CustomDiv } from '../../styles/GeralStyles';
+import './index.css';
 
 const Login = () => {
   return (
-    <div>
-      <InputText></InputText>
+    <CustomDiv
+      width="400px"
+      margin="auto"
+      border="1px solid #ddd"
+      marginTop="50px"
+    >
+      <div className="logo">
+        <h1>Login</h1>
+      </div>
+
+      <label htmlFor="email">Email</label>
+      <InputText id="email" placeholder="exemplo@exemplo.com"></InputText>
+
+      <label htmlFor="senha">Senha</label>
+      <InputText id="senha" placeholder="Digite sua senha"></InputText>
+
       <Button>Entrar</Button>
-    </div>
+
+      <div className='reset-password'>
+        <a href="#">Esqueci minha senha</a>
+      </div>
+    </CustomDiv>
   );
 };
 
