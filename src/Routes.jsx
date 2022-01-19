4c9b38b91f59952bbd02b21ff9e659c330b86499
Gabriel da Silva <gabrieldasilvaprivado1@gmail.com>
2022-01-19 20:03:43 -0300

    Add routes contact and about

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -4,17 +4,21 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './pages/Main';
 import Details from './pages/Details';
 import Login from './pages/Login';
+import Contact from './pages/Contact';
+import About from './pages/About';
 
 import Menu from './components/Menu';
 
 const MainRoutes = () => {
   return (
     <BrowserRouter>
-    <Menu></Menu>
+      <Menu></Menu>
       <Routes>
         <Route exact path="/" element={<Main />} />
         <Route path="/detalhes" element={<Details />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/contato" element={<Contact />} />
+        <Route path="/sobre" element={<About />} />
       </Routes>
     </BrowserRouter>
   );
