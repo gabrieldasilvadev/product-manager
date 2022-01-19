86512904c9413f594028d4e7841a1ff12fd6cd6a
Gabriel da Silva <gabrieldasilvaprivado1@gmail.com>
2022-01-19 19:42:36 -0300

    Delete header on App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import Header from './components/Header';
-
 import './Global.css';
 import MainRoutes from './Routes';
 
 function App() {
   return (
     <div className="app">
-      <Header></Header>
       <MainRoutes />
     </div>
   );
