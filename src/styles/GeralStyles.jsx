4fa2851c6679629a1e1222238410c487011b3831
Gabriel da Silva <gabrieldasilvaprivado1@gmail.com>
2022-01-19 15:37:52 -0300

    Update GeralStyles.jsx

--- src/styles/GeralStyles.jsx
+++ src/styles/GeralStyles.jsx
@@ -1,9 +1,9 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const CustomDiv = styled.div`
-  width: ${props => props.width};
-  border: ${props => props.border};
-  padding: 20px;
-  margin: ${props => props.margin};
-  margin-top: ${props => props.marginTop};
+  width: ${(props) => props.width};
+  border: ${(props) => props.border};
+  padding: 20px; 
+  margin: ${(props) => props.margin};
+  margin-top: ${(props) => props.marginTop};
 `;
