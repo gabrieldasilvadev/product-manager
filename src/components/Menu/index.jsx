ccf935e277b4ca95705632a7d06f55b80c9d2dce
Gabriel da Silva <gabrieldasilvaprivado1@gmail.com>
2022-01-19 20:11:46 -0300

    Render menu conditional

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -1,30 +1,36 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { Navbar } from '../../styles/GeralStyles';
 
 const Menu = () => {
   const logout = () => {
-    alert('Logout');
+    window.open('/login', '_self');
   };
 
-  return (
-    <Navbar>
-      <li>
-        <Link to={'/'}>Inicio</Link>
-      </li>
-      <li>
-        <Link to={'/contato'}>Contato</Link>
-      </li>
-      <li>
-        <Link to={'/sobre'}>Sobre</Link>
-      </li>
-      <li>
-        <a href="#" onClick={logout}>
-          Sair
-        </a>
-      </li>
-    </Navbar>
-  );
+  if (useLocation().pathname !== '/login') {
+    return (
+      <Navbar>
+        <li>
+          <Link to={'/'}>Inicio</Link>
+        </li>
+        <li>
+          <Link to={'/contato'}>Contato</Link>
+        </li>
+        <li>
+          <Link to={'/sobre'}>Sobre</Link>
+        </li>
+        <li>
+          <a href="#" onClick={logout}>
+            Sair
+          </a>
+        </li>
+      </Navbar>
+    );
+  } else {
+    return <div></div>
+  }
+
 };
 
 export default Menu;
