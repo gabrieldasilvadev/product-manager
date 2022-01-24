b6009aa36157c4dd8af4ce2237f42346c387095e
Gabriel da Silva <gabrieldasilvaprivado1@gmail.com>
2022-01-24 02:06:37 -0300

    Create service for validating validity

---
+++ src/service/UserService.jsx
@@ -0,0 +1,40 @@
+import Service from './Service';
+const userMock = {
+  id: 1,
+  nome: 'Gabriel',
+  email: 'gabriel@gmail.com',
+  perfil: {
+    id: 1,
+    descricao: 'manager',
+  },
+};
+
+function Login(email, senha) {
+  return new Promise((resolve, reject) => {
+
+    if(email != 'admin@admin.com' || senha != '123456') {
+      return reject('Usuário ou senha inválidos');
+    }
+
+    return resolve(userMock);
+  });
+}
+
+// Metodo real que vai na api tentar logar o usuario
+// function Login(email, senha) {
+//   return new Promise((resolve, reject) => {
+//     return Service.post('/api/login', { email, senha })
+//       .then((response) => resolve(response))
+//       .catch((error) => reject(error));
+//   });
+// }
+
+function GetAll() {
+  return new Promise((resolve, reject) => {
+    return Service.get('/api/users')
+      .then((response) => resolve(response))
+      .catch((error) => reject(error));
+  });
+}
+
+export default { Login, GetAll };
