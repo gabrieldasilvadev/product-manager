1b3934f5e79253773e2ce142c3c5c1b7ba935aee
Gabriel da Silva <gabrieldasilvaprivado1@gmail.com>
2022-01-27 12:32:19 -0300

    Update index.jsx

--- src/components/InputText/index.jsx
+++ src/components/InputText/index.jsx
@@ -5,7 +5,12 @@ const InputText = (props) => {
   return (
     <div>
       <label htmlFor={props.id}>{props.text}</label>
-      <Input id={props.id} placeholder={props.placeholder}></Input>
+      <Input
+        id={props.id}
+        placeholder={props.placeholder}
+        onChange={props.callback}
+        type={props.type}
+      ></Input>
     </div>
   );
 };
