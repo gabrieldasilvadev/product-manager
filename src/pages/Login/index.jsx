fba0d8d42217783b04aa33e9a2cc9bf9a8857b85
Gabriel da Silva <gabrieldasilvaprivado1@gmail.com>
2022-01-27 12:34:56 -0300

    Create function login for receive params email, password and create new user

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import InputText from '../../components/InputText';
 import { Button, Input } from '../../styles/FormStyles';
 import { CustomDiv } from '../../styles/GeralStyles';
 import UserService from '../../service/UserService';
+import Storage from '../../utils/Storage';
 
 import './index.css';
+import User from '../../model/User';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+
   const login = () => {
-    alert('tentar logar no sistema!');
+    UserService.Login(email, senha)
+      .then((response) => {
+        console.log(response);
+        const {token, usuario} = response;
+        Storage.saveToken(token);
+
+        Storage.saveUser(new User(usuario));
+        window.open('/', '_self');
+      })
+      .catch(error => {
+        console.log(error);
+        alert(error);
+      });
   };
 
   return (
@@ -26,12 +43,15 @@ const Login = () => {
         id="email"
         placeholder="seuemail@email.com"
         text="E-mail"
+        callback={(event) => setEmail(event.target.value)}
       ></InputText>
 
       <InputText
         id="senha"
         placeholder="Escreva a sua senha"
         text="Senha"
+        type="password"
+        callback={(event) => setSenha(event.target.value)}
       ></InputText>
 
       <Button onClick={login}>Entrar</Button>
