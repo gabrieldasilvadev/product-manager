bf35d21aa6eda2807fb1e7138d151aa8434d4170
Gabriel da Silva <gabrieldasilvaprivado1@gmail.com>
2022-01-27 12:33:23 -0300

    Create local storage manipulation

---
+++ src/utils/Storage.jsx
@@ -0,0 +1,30 @@
+import User from '../model/User';
+
+function saveToken(token) {
+  localStorage.setItem('token', token);
+}
+
+function getToken() {
+  localStorage.getItem('token');
+}
+
+function removeAuthendication() {
+  localStorage.removeItem('token');
+}
+
+function saveUser(usuario) {
+  let json = JSON.stringify(usuario);
+  localStorage.setItem('usuario', json);
+}
+
+function getUser() {
+  let json = localStorage.getItem('user');
+  return new User(JSON.parse(json));
+}
+
+export default { saveToken, 
+  getToken, 
+  removeAuthendication, 
+  saveUser, 
+  getUser 
+};
