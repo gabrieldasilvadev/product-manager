db443f4ae54587f7ffc7f770fb769427b3604e5c
Gabriel da Silva <gabrieldasilvaprivado1@gmail.com>
2022-01-27 14:39:59 -0300

    Create page Products Controoler

---
+++ src/pages/ProductsController/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './index.css';
+
+const ProductsController = () => {
+  return (
+    <div>
+      <h1>Controle de produtos</h1>
+    </div>
+  );
+};
+export default ProductsController;
