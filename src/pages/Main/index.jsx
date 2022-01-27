cd44aa8117438f408076c106be3aebd492be3ee8
Gabriel da Silva <gabrieldasilvaprivado1@gmail.com>
2022-01-27 14:19:06 -0300

    If user don't have token send to login page

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import Storage from '../../utils/Storage';
 import './index.css';
 
-export default class Main extends React.Component {
-  render() {
-    return (
-      <div className="main">
-        <h1>Pagina principal</h1>
-        <Link to="/detalhes">Detalhes</Link>
-      </div>
-    );
-  }
-}
+const Main = () => {
+
+  useEffect(() => {
+    let token = Storage.getToken();
+
+    if(!token) {
+      window.open('/login', '_self');
+    }
+  })
+
+  return (
+    <div className="main">
+      <h1>Pagina principal</h1>
+      <Link to="/detalhes">Detalhes</Link>
+    </div>
+  );
+};
+
+export default Main;
