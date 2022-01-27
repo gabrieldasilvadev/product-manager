a93fa0d08cc5877be220bf09eb53be20024e6596
Gabriel da Silva <gabrieldasilvaprivado1@gmail.com>
2022-01-27 17:37:44 -0300

    Create table of product controller and useEffect for response, map, error treatment

--- src/pages/ProductsController/index.jsx
+++ src/pages/ProductsController/index.jsx
@@ -1,10 +1,57 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './index.css';
+import ProductService from '../../service/ProductService';
+import Product from '../../model/Product';
 
 const ProductsController = () => {
+  const [products, setProducts] = useState(new Product());
+  let productsArr = [];
+
+  useEffect(() => {
+    ProductService.getAll()
+      .then(response => {
+        productsArr = response.data.map(product => new Product(product));
+        console.log(productsArr);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
-    <div>
-      <h1>Controle de produtos</h1>
+    <div className="container">
+      <div className="title">
+        <h1>Controle de Produtos</h1>
+      </div>
+
+      <div className="actions"></div>
+
+      <div className="product-table">
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Nome</th>
+              <th>Valor</th>
+              <th>Quantidade em Estoque</th>
+              <th>Data de cadastro</th>
+            </tr>
+          </thead>
+          <tbody>
+            {productsArr.map(product => {
+              (
+                <tr>
+                  <td>product.id</td>
+                  <td>product.nome</td>
+                  <td>product.valor</td>
+                  <td>product.quantidadeEstoque</td>
+                  <td>product.dataCadastro</td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
